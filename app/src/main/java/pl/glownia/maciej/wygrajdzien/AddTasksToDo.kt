17eadffa72c16f0bdb1a4aa91831c3cc03535c55
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-02-14 16:55:45 +0100

    Add skeleton version of activity where user can adding their tasks

---
+++ app/src/main/java/pl/glownia/maciej/wygrajdzien/AddTasksToDo.kt
@@ -0,0 +1,24 @@
+package pl.glownia.maciej.wygrajdzien
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.glownia.maciej.wygrajdzien.databinding.ActivityAddTasksToDoBinding
+
+class AddTasksToDo : AppCompatActivity() {
+    private var binding: ActivityAddTasksToDoBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddTasksToDoBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolbarAddTasksToDo)
+
+        if (supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        // Back button
+        binding?.toolbarAddTasksToDo?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
