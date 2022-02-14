59dc071e3e8438ee8542c086d126514680ab0e5c
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-02-14 16:51:50 +0100

    Set up  layout in MainActivity and in its xml file, so layout, also add viewBinding to gradle

--- app/src/main/java/pl/glownia/maciej/wygrajdzien/MainActivity.kt
+++ app/src/main/java/pl/glownia/maciej/wygrajdzien/MainActivity.kt
@@ -1,11 +1,21 @@
 package pl.glownia.maciej.wygrajdzien
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.glownia.maciej.wygrajdzien.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddTask?.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddTasksToDo::class.java)
+            startActivity(intent)
+        }
     }
 }
