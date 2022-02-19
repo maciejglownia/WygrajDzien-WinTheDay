ee4abd598c35f895d0a4b9277c1dfd43f00d6127
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-02-19 13:09:10 +0100

    Set up MainActivity as welcome activity

--- app/src/main/java/pl/glownia/maciej/wygrajdzien/MainActivity.kt
+++ app/src/main/java/pl/glownia/maciej/wygrajdzien/MainActivity.kt
@@ -1,8 +1,8 @@
 package pl.glownia.maciej.wygrajdzien
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import pl.glownia.maciej.wygrajdzien.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +13,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        binding?.fabAddTask?.setOnClickListener {
-            val intent = Intent(this@MainActivity, AddTasksToDo::class.java)
+        // Go to the next activity where is task list and possible to ask a new task
+        binding?.btnStart?.setOnClickListener {
+            val intent = Intent(this@MainActivity, TaskListActivity::class.java)
             startActivity(intent)
         }
     }
