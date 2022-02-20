28122b8ab15e46a803417fe2e70209ba330e28fc
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-02-20 19:45:10 +0100

    Add possibility to choose one of the available category by click on it and display chosen one

--- app/src/main/java/pl/glownia/maciej/wygrajdzien/activities/AddTaskToDoActivity.kt
+++ app/src/main/java/pl/glownia/maciej/wygrajdzien/activities/AddTaskToDoActivity.kt
@@ -21,25 +21,40 @@ class AddTaskToDoActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTasksToDoBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        setSupportActionBar(binding?.toolbarAddTasksToDo)
+        setSupportActionBar(binding?.toolbarAddTaskToDo)
 
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         // Back button
-        binding?.toolbarAddTasksToDo?.setNavigationOnClickListener {
+        binding?.toolbarAddTaskToDo?.setNavigationOnClickListener {
             onBackPressed()
         }
         // Set up save button
         binding?.btnSave?.setOnClickListener(this)
+        binding?.cvFirstCategoryField?.setOnClickListener(this)
+        binding?.cvSecondCategoryField?.setOnClickListener(this)
+        binding?.cvThirdCategoryField?.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         val taskDao = (application as TaskApp).db.taskDao()
         when (view!!.id) {
+            R.id.cvFirstCategoryField -> {
+                binding?.ivTaskChosenCategory?.setImageResource(R.drawable.sport)
+                binding?.ivTaskChosenCategory?.visibility = View.VISIBLE
+            }
+            R.id.cvSecondCategoryField -> {
+                binding?.ivTaskChosenCategory?.setImageResource(R.drawable.bulb_brain)
+                binding?.ivTaskChosenCategory?.visibility = View.VISIBLE
+            }
+            R.id.cvThirdCategoryField -> {
+                binding?.ivTaskChosenCategory?.setImageResource(R.drawable.money)
+                binding?.ivTaskChosenCategory?.visibility = View.VISIBLE
+            }
             R.id.btnSave -> {
                 when {
-                    binding?.etTitle?.text.isNullOrEmpty() -> {
+                    binding?.etTaskTitle?.text.isNullOrEmpty() -> {
                         Toast.makeText(this, "Wpisz nazwę zadania.", Toast.LENGTH_SHORT).show()
                     }
                     else -> {
@@ -55,7 +70,7 @@ class AddTaskToDoActivity : AppCompatActivity(), View.OnClickListener {
                             // updateTask(mTaskDetails!!.id, taskDao)
                         }
                         // Clear the entry for next adding
-                        binding?.etTitle?.text?.clear()
+                        binding?.etTaskTitle?.text?.clear()
                     }
                 }
                 // After click save button go to the main screen where is list of places
