a260126559cbcc4cd2e047fb431e156eeb21a01d
Maciej Głownia <maciej.k.glownia@gmail.com>
2022-02-21 21:53:32 +0100

    Add onClick method and OnItemClickListener with onItemClick to pass information

--- app/src/main/java/pl/glownia/maciej/wygrajdzien/adapter/TaskAdapter.kt
+++ app/src/main/java/pl/glownia/maciej/wygrajdzien/adapter/TaskAdapter.kt
@@ -7,27 +7,40 @@ import androidx.recyclerview.widget.RecyclerView
 import pl.glownia.maciej.wygrajdzien.database.TaskEntity
 import pl.glownia.maciej.wygrajdzien.databinding.ItemTaskBinding
 
-
-// We can now use here in < > -> PlaceAdapter.ViewHolder, which is this class that we created below
+// We can now use here in < > -> TaskAdapter.ViewHolder, which is this class that we created below
 // We need now to pass into a list that we want to use -> an array list in this case
 // TaskEntity is our model, which we are also using for our database
 class TaskAdapter(
     private val tasks: ArrayList<TaskEntity>,
+    private val onClickListener: OnItemClickListener
 ) :
     RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
     // Call a view holder, which is going to require a binding, which we can get from
-    // the items are all binding type or which will be of type ItemPlaceBinding
-    // why ItemPlaceBinding? Because that's how we called our example file - item_place.xml
+    // the items are all binding type or which will be of type ItemTaskBinding
+    // why ItemTaskBinding? Because that's how we called our example file - item_task.xml
     inner class ViewHolder(binding: ItemTaskBinding) :
         RecyclerView.ViewHolder(binding.root), View.OnClickListener {
         // Set up views from xml file
         val tvTaskTitle = binding.tvTaskTitle
         val ivTaskCategoryImage = binding.ivTaskCategoryImage
 
-        override fun onClick(p0: View?) {
-            TODO("Not yet implemented")
+        init {
+            binding.llMain.setOnClickListener(this)
         }
+
+        override fun onClick(v: View?) {
+            val position: Int = adapterPosition
+            val task = tasks[position]
+            if (position != RecyclerView.NO_POSITION) {
+                onClickListener.onItemClick(position, task)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        // Pass information you want to have
+        fun onItemClick(position: Int, entity: TaskEntity)
     }
 
     // When we create our ViewHolder we just return ViewHolder
@@ -47,12 +60,10 @@ class TaskAdapter(
         // Now we need to get an individual item and we get that from our items list
         // that is passed to this adapter
         // And we need to say at which position we want to have it because the item will be
-        // the individual item of our items, this which is going to be just an place entity item
-        val item = tasks[position] // this item will be an place entity object
+        // the individual item of our items, this which is going to be just an task entity item
+        val item = tasks[position] // this item will be an task entity object
 
-        // Get an image from the URI -> is what we get from our item image
-        // In this case, don't set the image based on a bitmap, but base it on URI
-        // So on the image that is stored inside of the phone
+        // It is going to be data as title of task and picture of category
         holder.tvTaskTitle.text = item.title
         // TODO("implement function to take drawable from database")
     }
